88dfc72114fce2cd158a94c6d160a07247376d06
Eliel Jesus G. V. <elieljgarciavela@gmail.com>
2025-10-01 14:21:27 +0200

    fix: fixed output and calculations

--- src/App.java
+++ src/App.java
@@ -1,8 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        double euros = 4.2;
+        double euros = 1;
         double valordepesos = 166.386;
 
-        System.out.printf(euros + " euros son " + (euros * valordepesos) + " pesetas.");
+        System.out.printf("%.2f euros son %.2f pesetas.", (float)euros, (euros * valordepesos));
     }
 }
