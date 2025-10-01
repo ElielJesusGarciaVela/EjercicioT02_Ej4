a862f7448cdaa286d7b8ee2d1bbf660899da424a
Eliel Jesus G. V. <elieljgarciavela@gmail.com>
2025-10-01 14:02:18 +0200

    first commit

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        double euros = 4.2;
+        double valordepesos = 166.386;
+
+        System.out.printf(euros + " euros son " + (euros * valordepesos) + ".");
+    }
+}
